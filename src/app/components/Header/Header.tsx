061223a28d24c491ccb257969852f65316c6ce33
Ugur Aral <uguraral95@gmail.com>
2023-09-19 23:10:46 +0300

    modified:   src/app/components/Header/Header.tsx 	modified:   src/app/globals.css

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,6 +1,14 @@
-import React, { useCallback } from "react";
+import React, { useCallback , useState  } from "react";
 
 
+export const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  //Handles the opening and closing of our nav
+  const handleClick = () => {
+      setIsOpen(!isOpen);
+  };
+}
 const Header = () => {
 const onFLMLERTextClick = useCallback(() => {
   // Please sync "filmler" to the project
@@ -19,12 +27,12 @@ return (
         alt=""
         src="/divider.svg"
       />
-      <div className="self-stretch flex flex-row items-center justify-between">
-        <img
-          src="/imgs/sinemaport-red-LOGO.png"
+      <div className="self-stretch flex md:flex-row flex-col items-center justify-between mx-auto">
+        <img className=" max-w-[15rem]"
+          src="/imgs/sinemaport-red-LOGO3x.png"
         />
-        <div className="flex flex-row items-center justify-end gap-[1.56rem]">
-          <div className="flex flex-row items-start justify-start gap-[2.38rem] text-[0.88rem]">
+        <div className="flex  md:flex-row flex-col items-center justify-end gap-[1.56rem] pt-4">
+          <div className="flex  md:flex-row flex-col items-start justify-start gap-[2.38rem] text-[0.88rem]">
             <div className="relative">ANASAYFA</div>
             <div
               className="relative font-medium cursor-pointer"
@@ -39,30 +47,30 @@ return (
               SEANSLAR
             </div>
           </div>
-          <div className="rounded-61xl bg-mcolor-red flex flex-col py-[0.38rem] px-[1rem] items-center justify-center">
+          <div className="flex flex-row gap-[1rem]">
+          <div className="rounded-3xl bg-[#D82121] flex flex-col py-[0.38rem] px-[1rem] items-center justify-center">
             <div className="flex flex-row items-center justify-center gap-[0.5rem]">
-              <div className="relative font-medium">Üye Ol</div>
-              <img
+              <div className="relative font-medium min-w-[3rem]">Üye Ol</div>
+              <img className=" max-w-[2rem]"
                 src="/icons/user.png"
               />
             </div>
           </div>
-          <div className="rounded-61xl bg-mcolor-red flex flex-col py-[0.38rem] px-[1rem] items-center justify-center">
+          <div className="rounded-3xl bg-[#D82121] flex flex-col py-[0.38rem] px-[1rem] items-center justify-center">
             <div className="flex flex-row items-center justify-center gap-[0.5rem]">
-              <div className="relative font-medium">Giriş Yap</div>
-              <img
+              <div className="relative font-medium min-w-[3rem]">Giriş Yap</div>
+              <img className=" max-w-[2rem]"
                 src="/icons/login.png"
               />
             </div>
           </div>
+          </div>
         </div>
       </div>
-      <img
-        className="self-stretch relative max-w-full overflow-hidden h-[0.06rem] shrink-0"
-        alt=""
-        src="/divider1.svg"
-      />
     </div>
+    <br/>
+    <hr className="border-[#D9D9D9]"/>
+    <br/>
     <div className="self-stretch rounded-3xl [background:linear-gradient(-90deg,_rgba(216,_33,_33,_0),_#d82121)] h-[3rem] flex flex-col py-[0.5rem] px-[1.5rem] box-border items-start justify-center text-[0.88rem] text-white">
       <div className="self-stretch flex flex-row items-center justify-start">
         <div className="flex-1 flex flex-row items-center justify-start gap-[2rem]">
